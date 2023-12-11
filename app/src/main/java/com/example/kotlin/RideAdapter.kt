90656e6b33ab25d2f300d4e95317a0dc8d377f93
emreerkus <emre.erkus@ozu.edu.tr>
2023-12-12 00:33:04 +0300

    final

---
+++ app/src/main/java/com/example/kotlin/RideAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.kotlin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RideAdapter(private var rideList: List<RideModel>) : RecyclerView.Adapter<RideAdapter.RideViewHolder>() {
+
+    class RideViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvSource: TextView = view.findViewById(R.id.tvSource)
+        val tvDestination: TextView = view.findViewById(R.id.tvDestination)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RideViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_ride, parent, false)
+        return RideViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RideViewHolder, position: Int) {
+        val ride = rideList[position]
+        // holder.textViewName.text = ride.empName // Bind data to your views
+        holder.tvSource.text = ride.empSource // Assuming empSource is the source field in RideModel
+        holder.tvDestination.text = ride.empDestination // Assuming empDestination is the destination field in RideModel
+        // Bind other data
+    }
+
+    override fun getItemCount(): Int = rideList.size
+
+    fun updateData(newList: List<RideModel>) {
+        rideList = newList
+        notifyDataSetChanged()
+    }
+}
